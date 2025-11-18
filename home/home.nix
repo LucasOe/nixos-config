1809cfb2a1d40eae6ecfe92575142ef71e8407b4
LucasOe <github@lucasoe.com>
2025-11-18 20:42:26 +0100

    Add XDG configuration to manage desktop entries

--- home/home.nix
+++ home/home.nix
@@ -33,6 +33,26 @@
     devenv
   ];
 
+  # XDG
+  xdg = {
+    enable = true;
+    # Hide desktop entries
+    desktopEntries = {
+      qt5ct = {
+        name = "Qt5 Settings";
+        noDisplay = true;
+      };
+      qt6ct = {
+        name = "Qt6 Settings";
+        noDisplay = true;
+      };
+      kvantummanager = {
+        name = "Kvantum Manager";
+        noDisplay = true;
+      };
+    };
+  };
+
   # Git
   programs.git = {
     enable = true;

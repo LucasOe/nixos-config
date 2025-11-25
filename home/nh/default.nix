8588c58183a3386ca37390b411c2946d2b18a1c0
LucasOe <github@lucasoe.com>
2025-11-25 07:52:55 +0100

    Move flake to home dir

--- home/nh/default.nix
+++ home/nh/default.nix
@@ -5,6 +5,6 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep 3";
-    flake = "/etc/nixos/"; # TODO: Move flake to home dir
+    flake = "/home/lucas/dev/nixos-config";
   };
 }

6d75ac15cd167fd4773254721ad334333a693367
LucasOe <github@lucasoe.com>
2025-12-27 19:45:28 +0100

    Change to beta nvidia branch and enable modesetting

--- hosts/tamas/configuration.nix
+++ hosts/tamas/configuration.nix
@@ -1,4 +1,9 @@
-{ username, pkgs, ... }:
+{
+  config,
+  pkgs,
+  username,
+  ...
+}:
 
 {
   imports = [
@@ -45,12 +50,16 @@
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
   };
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
     # Support is limited to the Turing and later architectures. Full list of
@@ -58,6 +67,8 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     open = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.

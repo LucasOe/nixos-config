f78ca1b39227ee7ffc738a2a711e13f54db8fd87
LucasOe <github@lucasoe.com>
2026-01-01 21:29:15 +0100

    Set system.stateVersion per host

--- hosts/tamas/configuration.nix
+++ hosts/tamas/configuration.nix
@@ -85,9 +85,11 @@
       "wheel"
       "docker"
     ];
+  };
 
-    # Home packages
-    packages = with pkgs; [
+  # User Packages
+  home-manager.users.${username} = {
+    home.packages = with pkgs; [
       gdlauncher-carbon
       cartridges
     ];
@@ -105,4 +107,12 @@
   services.udev.packages = with pkgs; [
     via
   ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 }

3e58b16784a175b4cc4ce3d986af24bd18442008
LucasOe <github@lucasoe.com>
2026-01-18 09:01:12 +0100

    Enable flatpak for all hosts

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -135,4 +135,7 @@
     Defaults pwfeedback # password input feedback - makes typed password visible as asterisks
     Defaults timestamp_timeout=120 # only ask for password every 2h
   '';
+
+  # Enable Flatpak
+  services.flatpak.enable = true;
 }

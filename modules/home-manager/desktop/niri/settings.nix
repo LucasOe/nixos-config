891d6c477c0639c4b3379e7c9e16985b8997a410
LucasOe <github@lucasoe.com>
2026-01-27 20:38:14 +0100

    Remove NIXOS_OZONE_WL environment variable from niri settings

--- modules/home-manager/desktop/niri/settings.nix
+++ modules/home-manager/desktop/niri/settings.nix
@@ -91,7 +91,6 @@
         GDK_BACKEND = "wayland";
         GSK_RENDERER = "ngl";
         MOZ_ENABLE_WAYLAND = "1";
-        NIXOS_OZONE_WL = "1";
         QT_QPA_PLATFORM = "wayland";
         QT_QPA_PLATFORMTHEME = "gtk3";
         QT_QPA_PLATFORMTHEME_QT6 = "gtk3";

e58cb145cca40bf97626c65924aff2513487b635
LucasOe <github@lucasoe.com>
2026-01-27 20:25:04 +0100

    niri: update environment settings

--- modules/home-manager/desktop/niri/settings.nix
+++ modules/home-manager/desktop/niri/settings.nix
@@ -87,12 +87,14 @@
 
       environment = {
         CLUTTER_BACKEND = "wayland";
+        ELECTRON_OZONE_PLATFORM_HINT = "auto";
         GDK_BACKEND = "wayland";
         GSK_RENDERER = "ngl";
         MOZ_ENABLE_WAYLAND = "1";
         NIXOS_OZONE_WL = "1";
         QT_QPA_PLATFORM = "wayland";
-        QT_QPA_PLATFORMTHEME = "qt6ct";
+        QT_QPA_PLATFORMTHEME = "gtk3";
+        QT_QPA_PLATFORMTHEME_QT6 = "gtk3";
         QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
         XDG_CURRENT_DESKTOP = "niri";
         XDG_SESSION_DESKTOP = "niri";

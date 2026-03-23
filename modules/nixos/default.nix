8076b12bcbc3fc834fe3ed46fa4506f8abaf0286
LucasOe <github@lucasoe.com>
2026-03-23 14:48:56 +0100

    Add binary cache for noctalia to extra-substituters

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -48,13 +48,15 @@
 
     # Add substituters
     extra-substituters = [
-      "https://nix-community.cachix.org"
       "https://niri.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://noctalia.cachix.org"
       "https://zed.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
     ];
 

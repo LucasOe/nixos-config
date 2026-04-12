164e7fd3451f92d55d838b88ea06397005363c91
LucasOe <github@lucasoe.com>
2026-04-12 20:59:16 +0200

    Remove substituter for zed

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -50,12 +50,10 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://noctalia.cachix.org"
-      "https://zed.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
     ];
 
     # Add sudo users as trusted, so nixos-rebuild works

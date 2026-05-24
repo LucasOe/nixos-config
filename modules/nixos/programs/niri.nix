4653e9b2cc0a2306f8bd11bd7d5b7029becbea1f
LucasOe <github@lucasoe.com>
2026-05-24 22:02:18 +0200

    niri: remove settings already set by programs.niri

--- modules/nixos/programs/niri.nix
+++ modules/nixos/programs/niri.nix
@@ -1,30 +1,17 @@
 { inputs, pkgs, ... }:
 
 {
+  # Can be removed once https://github.com/NixOS/nixpkgs/pull/442948 is merged
   environment.systemPackages = with pkgs; [
     xwayland-satellite
   ];
 
-  # Enable Niri
+  # Enable niri
+  # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/programs/wayland/niri.nix
   programs.niri.enable = true;
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
   programs.niri.package = pkgs.niri-unstable;
 
-  # Required by Niri
-  security.polkit.enable = true; # polkit
-  services.gnome.gnome-keyring.enable = true; # secret service
-
   # Disable KDE polkit agent from niri-flake
   systemd.user.services.niri-flake-polkit.enable = false;
-
-  # XDG Portals
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-    ];
-  };
 }

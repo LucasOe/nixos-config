9b5f3f523274dc8a94b0393861d83c8d61f32b93
LucasOe <github@lucasoe.com>
2026-06-12 17:10:39 +0200

    proton-cachyos-bin: 11.0-20260521 -> 11.0-20260601

--- overlays/proton-cachyos-bin.nix
+++ overlays/proton-cachyos-bin.nix
@@ -3,11 +3,11 @@
     (final: prev: {
       proton-cachyos-bin = final.stdenvNoCC.mkDerivation (finalAttrs: {
         pname = "proton-cachyos-bin";
-        version = "11.0-20260521";
+        version = "11.0-20260601";
 
         src = final.fetchzip {
           url = "https://github.com/CachyOS/proton-cachyos/releases/download/cachyos-${finalAttrs.version}-slr/proton-cachyos-${finalAttrs.version}-slr-x86_64.tar.xz";
-          hash = "sha256-OrZfLv0FV+fhDuhbmcbMhtylDTMwTfRAvENh/v7iIr8=";
+          hash = "sha256-WkQRPX7sw/OnphLek5XGSrJTY8hrECCJY9zUaw6/jdA=";
         };
 
         dontUnpack = true;

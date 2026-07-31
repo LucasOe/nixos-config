dccb21b2b554c868d97c369722132e10d30f32ef
LucasOe <github@lucasoe.com>
2026-07-31 12:59:36 +0200

    Disable plymouth and silent boot

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -3,19 +3,21 @@
 {
   boot = {
     # Enable Plymouth
-    plymouth.enable = true;
+    # plymouth.enable = true;
 
     # Enable "Silent boot"
     # https://wiki.nixos.org/wiki/Plymouth
     # https://wiki.archlinux.org/title/Silent_boot
-    consoleLogLevel = 3;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      # "splash" # Already set by plymouth.enable
-      "rd.udev.log_level=3"
-      "rd.systemd.show_status=auto"
-    ];
+    # consoleLogLevel = 3;
+    # initrd.verbose = false;
+    # kernelParams = [
+    #   "quiet"
+    #   # "splash" # Already set by plymouth.enable
+    #   "rd.udev.log_level=3"
+    #   "rd.systemd.show_status=auto"
+    #   "vt.global_cursor_default=0"
+    #   "fbcon=nodefer"
+    # ];
 
     # Bootloader
     loader = {

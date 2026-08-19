e4548df3459991fbd4ad5f039a4a2c1ce7433850
LucasOe <github@lucasoe.com>
2026-08-19 19:48:07 +0200

    libratbag: update libratbag and piper packages

--- overlays/libratbag.nix
+++ overlays/libratbag.nix
@@ -2,24 +2,24 @@
   nixpkgs.overlays = [
     (final: prev: {
       libratbag = prev.libratbag.overrideAttrs (_: {
-        version = "unstable-2025-12-28";
+        version = "unstable-2026-08-18";
 
         src = final.fetchFromGitHub {
           owner = "libratbag";
           repo = "libratbag";
-          rev = "c5214d26336695d1414a6b4e32697f5b5a835089";
-          hash = "sha256-o1lsdJ8QwprpBVUdntjjfV6lPaqhG0rD9ySjf+LXriQ=";
+          rev = "b8d4d3ca1f4d6b23c664ffee2888b8eb669bee21";
+          hash = "sha256-8V/LIki/tI/9Wi6kuFJp6k1p+moMh8Gc8RNP1BUlZO8=";
         };
       });
 
       piper = prev.piper.overrideAttrs (_: {
-        version = "unstable-2025-12-28";
+        version = "unstable-2026-08-12";
 
         src = final.fetchFromGitHub {
           owner = "libratbag";
           repo = "piper";
-          rev = "48056eb1788cdfbdda67a2360c53b7157782a9b3";
-          hash = "sha256-jF7qowV4Ub5MqQBlTDuf7x83RTfMQ/J5DSD74lP02pU=";
+          rev = "5d3c8845b55643595ccb8029dfa5c7a2fb079e77";
+          hash = "sha256-oGa0NXgoiVYadCAB5cnQYrjh3KIVWhTxsSYaB4TVZSA=";
         };
       });
     })

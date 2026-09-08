7efc04cfad763030ced87410ad8ae9c5e04c9279
LucasOe <github@lucasoe.com>
2026-09-08 17:37:38 +0200

    xwayland-satellite: update overlay PR

--- overlays/xwayland-satellite.nix
+++ overlays/xwayland-satellite.nix
@@ -8,8 +8,8 @@
           # fix steam dropdowns closing instantly and unity add component not focusing
           # https://github.com/Supreeeme/xwayland-satellite/pull/494
           (final.fetchpatch2 {
-            url = "https://github.com/Supreeeme/xwayland-satellite/compare/9d51b59ff3c38464e7654096c9b10a8052a26b25.diff?full_index=1";
-            hash = "sha256-VpdX1V9N0pkBJoRuqTwZiwJeW4h200TdsPN75xnBSEk=";
+            url = "https://github.com/Supreeeme/xwayland-satellite/compare/22cde7d2883a846f26fee8b7a26994f7e08e7a94.diff?full_index=1";
+            hash = "sha256-E5AWhsCJreFtDSd+CFqLWZI2CqN9K2FXeI9Cus9x8xE=";
           })
         ];
       });

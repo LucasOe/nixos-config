2dcc8b6303e2a5e08ea9bd7333233feee3b4e8ea
LucasOe <github@lucasoe.com>
2026-09-15 22:47:47 +0200

    fish: make dependent on `nixosConfig.program.fish`

--- modules/home-manager/fish/default.nix
+++ modules/home-manager/fish/default.nix
@@ -1,18 +1,36 @@
-{ ... }:
-
 {
-  programs.fish = {
-    enable = true;
-    generateCompletions = false; # Completions don't work when set to true for some reason
-    interactiveShellInit = ''
-      set fish_greeting # Disable greeting
-    '';
-    binds = {
-      # Binds are removed from the shell completions by nixpkgs and have to be set manually
-      # https://github.com/alexpasmantier/television/blob/main/television/utils/shell/completion.fish#L140
-      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/te/television/package.nix#L50
-      # "ctrl-t".command = "tv_smart_autocomplete";
-      # "ctrl-r".command = "tv_shell_history";
+  config,
+  lib,
+  nixosConfig,
+  ...
+}:
+
+let
+  cfg = config.my.fish;
+in
+{
+  options.my.fish = {
+    enable = lib.mkEnableOption "fish" // {
+      default = nixosConfig.programs.fish.enable;
+      defaultText = lib.literalExpression "nixosConfig.programs.fish.enable";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.fish = {
+      enable = true;
+
+      generateCompletions = false; # Completions don't work when set to true for some reason
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      binds = {
+        # Binds are removed from the shell completions by nixpkgs and have to be set manually
+        # https://github.com/alexpasmantier/television/blob/main/television/utils/shell/completion.fish#L140
+        # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/te/television/package.nix#L50
+        # "ctrl-t".command = "tv_smart_autocomplete";
+        # "ctrl-r".command = "tv_shell_history";
+      };
     };
   };
 }

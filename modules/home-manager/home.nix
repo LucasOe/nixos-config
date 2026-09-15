88b40f7a3ec8a680b0391410471c1f07324cace4
LucasOe <github@lucasoe.com>
2026-09-16 00:56:26 +0200

    Remove remaining unused packages

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -5,41 +5,28 @@
   ...
 }:
 
-let
-  cliPackages = with pkgs; [
-    biome
-    nil
-    nixd
-    nixfmt
-    oxfmt
-    oxlint
-    sqlite
-    stylua
-    trashy
-  ];
-
-  guiPackages = with pkgs; [
-    baobab
-    bazaar
-    decibels
-    eyedropper
-    file-roller
-    gnome-calculator
-    gnome-characters
-    gnome-connections
-    gnome-disk-utility
-    gnome-font-viewer
-    gnome-logs
-    loupe
-    obsidian
-    papers
-    refern
-    resources
-    showtime
-    simple-scan
-    snapshot
-  ];
-in
 {
-  home.packages = cliPackages ++ lib.optionals nixosConfig.my.gui.enable guiPackages;
+  config = lib.mkIf nixosConfig.my.gui.enable {
+    home.packages = with pkgs; [
+      baobab
+      bazaar
+      decibels
+      eyedropper
+      file-roller
+      gnome-calculator
+      gnome-characters
+      gnome-connections
+      gnome-disk-utility
+      gnome-font-viewer
+      gnome-logs
+      loupe
+      obsidian
+      papers
+      refern
+      resources
+      showtime
+      simple-scan
+      snapshot
+    ];
+  };
 }

48a96c94489e64d778cc94c24dcce382f12dbf65
LucasOe <github@lucasoe.com>
2026-09-16 08:44:48 +0200

    fish: enable `generateCompletions`

--- modules/home-manager/fish/default.nix
+++ modules/home-manager/fish/default.nix
@@ -20,7 +20,6 @@ in
     programs.fish = {
       enable = true;
 
-      generateCompletions = false; # Completions don't work when set to true for some reason
       interactiveShellInit = ''
         set fish_greeting # Disable greeting
       '';
